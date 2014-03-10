5e39cec1cbe2dae95b9272e22c5eaf2507730c6b
Brian Barnekow <bbarnekow@nvisia.com>
2014-03-10 14:42:39 -0500

    Exercise 6 Solution

--- webapp/js/services.js
+++ webapp/js/services.js
@@ -2,24 +2,6 @@
  * Created by bpeterson on 1/27/14.
  */
 
-var restaurantServices = angular.module('restaurantServices', []);
-
-restaurantServices.factory('Restaurant', ['$http',
-    function ($http) {
-        return {
-            GetAllRestaurants: function () {
-                return $http.get("/restaurants");
-            },
-            GetRestaurant: function (attrs) {
-                return $http.get("/restaurants/"+attrs.restaurantId);
-            },
-            GetReservations: function (attrs) {
-                return $http.get("/restaurants/"+attrs.restaurantId+"/reservations");
-            }
-        };
-    }]);
-
-/*
 var restaurantServices = angular.module('restaurantServices', ['ngResource']);
 
 restaurantServices.factory('Restaurant', ['$resource',
@@ -35,4 +17,4 @@ restaurantServices.factory('Reservation', ['$resource',
     function ($resource) {
         return $resource('/reservations/:reservationId', {reservationId: '@reservationId'});
     }
-]);*/
+]);
