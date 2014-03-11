46552e8fc7b8856bbb77932566474b1c01961f80
ezunke <ezunke@nvisia.com>
2014-03-11 11:02:58 -0500

    Added license and disclaimer comments

--- webapp/js/services.js
+++ webapp/js/services.js
@@ -1,20 +1,40 @@
-/**
- * Created by bpeterson on 1/27/14.
+/*
+ * The MIT License (MIT)
+ *
+ *  Copyright (c) 2014 © NVISIA, LLC.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
  */
 
-var restaurantServices = angular.module('restaurantServices', ['ngResource']);
-
-restaurantServices.factory('Restaurant', ['$resource',
-    function ($resource) {
-        return $resource('/restaurants/:restaurantId/:reservations', {restaurantId: '@restaurantId'}, {
-            GetRestaurant: {method: 'GET', params: {restaurantId: 'restaurantId'}, cache: true},
-            GetAllRestaurants: {method: 'GET', params: {}, isArray: true},
-            GetReservations: {method: 'GET', params: {restaurantId: 'restaurantId', reservations: 'reservations'}}
-        });
-    }]);
-
-restaurantServices.factory('Reservation', ['$resource',
-    function ($resource) {
-        return $resource('/reservations/:reservationId', {reservationId: '@reservationId'});
-    }
-]);
+//var restaurantServices = angular.module('restaurantServices', ['ngResource']);
+//
+//restaurantServices.factory('Restaurant', ['$resource',
+//    function ($resource) {
+//        return $resource('/restaurants/:restaurantId/:reservations', {restaurantId: '@restaurantId'}, {
+//            GetRestaurant: {method: 'GET', params: {restaurantId: 'restaurantId'}, cache: true},
+//            GetAllRestaurants: {method: 'GET', params: {}, isArray: true},
+//            GetReservations: {method: 'GET', params: {restaurantId: 'restaurantId', reservations: 'reservations'}}
+//        });
+//    }]);
+//
+//restaurantServices.factory('Reservation', ['$resource',
+//    function ($resource) {
+//        return $resource('/reservations/:reservationId', {reservationId: '@reservationId'});
+//    }
+//]);
