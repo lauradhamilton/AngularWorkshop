555e4ecaa36abdd1612c973f3f1c7d896370da6e
ezunke <ezunke@nvisia.com>
2014-03-11 16:40:18 -0500

    changed name of the controllers module

--- webapp/js/controllers.js
+++ webapp/js/controllers.js
@@ -26,7 +26,7 @@
 
 // Create bootstrap module based on ng-app name.
 // First argument matches the ng-app directive in index.html.
-var restaurantReservationModule = new angular.module('restaurantReservationApp', []);
+var restaurantReservationModule = new angular.module('restaurantReservationControllers', []);
 
 // RestaurantListController is the ng-controller value in index.html. It identifies the DOM element that this controller 'controls'
 restaurantReservationModule.controller('RestaurantListController', function ($scope) {
